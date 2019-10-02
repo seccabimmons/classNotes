993544ade97572f56c6a8e97301d8ebf31ba1ae9
seccabimmons <auntgriz@gmail.com>
2019-10-02 17:18:24 -0500

    js notes

---
+++ loopNotes.js
@@ -0,0 +1,31 @@
+var a = 1;
+var b = 2;
+var k = 10;
+var j = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var v = "JavaScript";
+
+
+
+for(i = 0; i < j.length; i++) {
+    console.log("the value of I is : " + i)   
+}
+
+for (i = 0; i < 25; i++) {
+    var text = "";
+    text += "Becca";
+    console.log(text);
+}
+
+var text = "";
+var i = 0;
+    while(i < 10) {
+        text += "The number is " + i;
+        i++;
+    }
+
+    var text = ["", "", ""];
+    for (var i = 0; i < text.length; i++)
+    {
+        text += "The number is " + i;
+        console.log(text);
+    }
---
+++ vocabulary.js
@@ -0,0 +1,23 @@
+// function - a set of statements that perform a task
+// onload - an event that triggers when the script loads
+// .innerHTML - allows you to insert content to a specific id
+// script - html tag where you put javascript or link to a js file
+// html - hypertext markup language
+// body - html element that holds the main content for the document
+// event - when something happens to the page like loading or clicking a button
+// alert - creates a pop up box
+// handlers - defines what will happen when an event is triggered
+// onclick - event that's triggered when something is clicked
+// data - information provided by a function
+// console - developer tool that can display information and helps check for errors
+// boolean - data type that shows if a statement is true or false
+// number - data type that only contains numbers
+// += - assignment operator that does the addition and assigns the value
+// /= - assignment operator that does the division operation and assigns the value
+// == - relational operator that's used to see if 2 operands are equal
+// value - data represented by a variable
+// getElementById - returns the element by the specified id
+// if - statement that, if the condition is met, the following code will run
+// else - statement that, if the condition was not met, this code will run instead
+// constant - a variable with "const" before it. it cannot be changed
+// var - a variable that holds a data type
